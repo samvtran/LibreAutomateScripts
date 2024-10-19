59d46fce512aa9802fe48eeb25573f7079a65509
Sam Tran <sam@samvtran.com>
2024-10-19 07:18:39 -0700

    capacities: add heading levels

--- Functions/Capacities.cs
+++ Functions/Capacities.cs
@@ -1,37 +1,49 @@
 using Au.Triggers;
 
-partial class Program {
-	/// <summary>
-	/// Custom shortcuts for the Capacities PKM app.
-	/// </summary>
-	void CapacitiesShortcuts(HotkeyTriggers hk) {
-		Triggers.Of.Window("Capacities");
+partial class Program
+{
+    /// <summary>
+    /// Custom shortcuts for the Capacities PKM app.
+    /// </summary>
+    void CapacitiesShortcuts(HotkeyTriggers hk)
+    {
+        Triggers.Of.Window("Capacities");
 
-		// Select the add/new button since the regular Ctrl+N shortcut is for creating any object not just the one focused.
-		hk["Ctrl+Shift+N"] = o => {
-			var w = wnd.active;
-			var button = w.Elm["web:BUTTON", also: o => o.Name == "Add" || o.Name == "New"];
+        // Select the add/new button since the regular Ctrl+N shortcut is for creating any object not just the one focused.
+        hk["Ctrl+Shift+N"] = o =>
+        {
+            var w = wnd.active;
+            var button = w.Elm["web:BUTTON", also: o => o.Name == "Add" || o.Name == "New"];
 
-			if (!button.Exists()) return;
+            if (!button.Exists()) return;
 
-			button.Find(1).Invoke();
-		};
+            button.Find(1).Invoke();
+        };
 
-		// Activate the block-level color picker
-		hk["Ctrl+Alt+Shift+H"] = o => {
-			var w = wnd.active;
-			// Trigger the block-level editor panel
-			keys.send("Ctrl+Shift+E");
+        // Activate the block-level color picker
+        hk["Ctrl+Alt+Shift+H"] = o =>
+        {
+            var w = wnd.active;
+            // Trigger the block-level editor panel
+            keys.send("Ctrl+Shift+E");
 
-			var blockButton = w.Elm["web:GROUPING", "Turn blocks into"];
-			if (!blockButton.Exists(2)) return;
+            var blockButton = w.Elm["web:GROUPING", "Turn blocks into"];
+            if (!blockButton.Exists(2)) return;
 
-			var colorButton = blockButton.Find().Navigate("parent previous first");
-			if (colorButton == null) return;
+            var colorButton = blockButton.Find().Navigate("parent previous first");
+            if (colorButton == null) return;
 
-			colorButton.MouseClick();
-		};
+            colorButton.MouseClick();
+        };
 
-		Triggers.Of.AllWindows();
-	}
+        // Alt+1-4 for h1-h4
+        foreach (var i in Enumerable.Range(1, 4)) {
+            hk[$"Alt+{i}"] = o => {
+                keys.send("Space", "/", "h", $"{i}", 100);
+                keys.send("Enter", "Backspace");
+            };
+        }
+
+        Triggers.Of.AllWindows();
+    }
 }
